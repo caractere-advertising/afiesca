6c1c20a45c21dcd0fdd04cd481a39667d99ac68e
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-02-06 17:00:09 +0100

    Ajout bg header par défaut

--- services.php
+++ services.php
@@ -2,14 +2,24 @@
 /* Template Name: Services */
 
 get_header();
+
 $bg_header = get_field('background');
 $title_header = get_field('titre_page');
-$linkf = get_field('link_to_form');?>
+$linkf = get_field('link_to_form');
+
+if(!$bg_header):
+    $bg_url = get_template_directory_uri(  ).'/assets/img/bg-default.jpg';
+else :
+    $bg_header = get_field('background');
+    $bg_url = $bg_header['url'];
+endif;?>
+
+
 
 <?php get_template_part( 'templates-parts/header-nav');?>
 
 
-<header id="header" style="background:url('<?php if($bg_header): echo $bg_header['url']; endif;?>');">
+<header id="header" style="background:url('<?php if($bg_url): echo $bg_url; endif;?>');">
 </header>
 
 <section id="intro_service">
