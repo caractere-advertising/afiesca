6a7392cbcc8099a31c4768591414f2cbb10a98d9
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-02-08 14:47:34 +0100

    Ajout navigation menu hamburger

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,6 +1,9 @@
 var hamburger = document.querySelector(".hamburger-menu");
 var menu = document.querySelector(".megamenu");
+var header = document.querySelector(".navigation");
 
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("active");
+  header.classList.toggle("active");
+  header.classList.toggle("fixed");
 });
