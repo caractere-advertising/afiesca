0bd8bc636bc3c7febfe9da4314f785a95aecb485
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-03-06 09:45:54 +0100

    ajout titre

--- templates-parts/section-mots-president.php
+++ templates-parts/section-mots-president.php
@@ -2,8 +2,14 @@
     <div class="container">
         <div class="columns">
             <div class="col-g">
-                <?php $img = get_field('image_nossolutions','options');?>
-                <?php if($img):?>
+                <?php $img = get_field('image_nossolutions','options');
+                
+                if(is_page(318)):
+                    $txtPresident = get_field('texte_du_president','options');
+                endif;
+                
+                if($img):?>
+                    
                 <img src="<?php echo $img['url'];?>" alt="<?php echo $img['name'];?>" class="from-left" />
                 <?php endif;?>
             </div>
@@ -11,7 +17,9 @@
                 <?php 
                 $txtPresident = get_field('texte_du_president','options');
                 
-                if($txtPresident): echo $txtPresident; endif;?>
+                if(!is_page(318)):
+                    if($txtPresident): echo $txtPresident; endif;
+                endif;?>
             </div>
         </div>
     </div>
