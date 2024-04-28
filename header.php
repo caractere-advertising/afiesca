b2db1e696f2833ba26b74fc6e5f963aa4f2a4a4b
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-28 12:15:40 +0200

    header.php

--- header.php
+++ header.php
@@ -8,11 +8,10 @@
 
     <link rel="stylesheet" href="https://use.typekit.net/ovg4lmv.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script src="https://kit.fontawesome.com/53b095485a.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/interactiveimagejs@2.7.1/dist/interactive-image.min.css" />
 
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://kit.fontawesome.com/53b095485a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script src="<?php echo get_template_directory_uri();?>/dist/main.js" defer></script>
