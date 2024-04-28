3835ab1527666315e0ffc0466051dd39eff740c3
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-28 12:09:48 +0200

    corr

--- partenaires.php
+++ partenaires.php
@@ -26,7 +26,7 @@ get_template_part( 'templates-parts/header-nav');?>
         <?php $images = get_field('images-map');
 
         if($images):?>
-            <div id="my-interactive-image" style="background:url('<?php echo $images['url'];?>'); width:100%;background-size:contain;height:700px;background-repeat:no-repeate;background-position:center"></div>
+            <div id="my-interactive-image" style="background:url('<?php echo $images['url'];?>'); width:100%;background-size:contain;height:700px;background-repeat:no-repeat;background-position:center"></div>
         <?php endif;?>
 
         <div class="result">
