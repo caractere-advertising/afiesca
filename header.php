e4b2ee96811b8dc35b70c8054b01663d35d47c55
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-28 14:17:39 +0200

    ajout js

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://kit.fontawesome.com/53b095485a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/interactiveimagejs@2.7.1/dist/interactive-image.min.js"></script>
 
     <script src="<?php echo get_template_directory_uri();?>/dist/main.js" defer></script>
 
