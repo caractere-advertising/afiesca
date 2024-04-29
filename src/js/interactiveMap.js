6468b5f4b25b5ab53f0c40113999f6842525be62
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 12:29:16 +0200

    ajout fonction calcul responsive

---
+++ src/js/interactiveMap.js
@@ -0,0 +1,23 @@
+// Fonction pour mettre à jour les positions des hotspots en fonction de la taille de l'image
+function updateHotspotsPosition() {
+    var imageWidth = $('#my-interactive-image').width();
+    var imageHeight = $('#my-interactive-image').height();
+
+    // Mettre à jour les positions des hotspots en fonction de la taille de l'image
+    $('.hotspot').each(function() {
+        var left = parseInt($(this).css('left'));
+        var top = parseInt($(this).css('top'));
+
+        // Convertir les positions en pourcentage
+        var newLeft = (left / imageWidth) * 100 + '%';
+        var newTop = (top / imageHeight) * 100 + '%';
+
+        // Mettre à jour les positions des hotspots
+        $(this).css({ 'left': newLeft, 'top': newTop });
+    });
+}
+
+// Appeler la fonction de mise à jour des hotspots lors du chargement de la page et lors du redimensionnement de la fenêtre
+$(window).on('load resize', function() {
+    updateHotspotsPosition();
+});
