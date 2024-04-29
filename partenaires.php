fcb1b4b7f4892a7c819b960f8917cb46326f9318
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 09:46:14 +0200

    AJOUT JQ

--- partenaires.php
+++ partenaires.php
@@ -78,34 +78,33 @@ get_template_part( 'templates-parts/header-nav');?>
 
 
 <script>
-// Items collection
+    // Items collection
+    $(document).ready(function() {
+        var items = <?php echo json_encode($items); ?>;
 
+        var options = {
+            allowHtml: true,
+            triggerEvent: 'click',
+            shareBox: false,
+        }
+        
+        // Plugin activation
+        $("#my-interactive-image").interactiveImage(items, options);
 
-    var items = <?php echo json_encode($items); ?>;
+        // Gestionnaire d'événements pour afficher les informations du client
+        $('#my-interactive-image').on('click', '.interactive-point', function() {
+            var index = $(this).index();
+            var clientInfo = items[index];
+            $('#client-info').html('<h3>' + clientInfo.title + '</h3><p>' + clientInfo.description + '</p>').show();
+        });
 
-  var options = {
-    allowHtml: true,
-    triggerEvent: 'click',
-    shareBox: false,
-  }
-  
-  // Plugin activation
-  $("#my-interactive-image").interactiveImage(items, options);
-
-// Gestionnaire d'événements pour afficher les informations du client
-$('#my-interactive-image').on('click', '.interactive-point', function() {
-    var index = $(this).index();
-    var clientInfo = items[index];
-    $('#client-info').html('<h3>' + clientInfo.title + '</h3><p>' + clientInfo.description + '</p>').show();
-});
-
-// Cachez les informations du client lorsqu'on clique en dehors des marqueurs
-$('#map-container').on('click', function(event) {
-    if (!$(event.target).closest('.interactive-point').length) {
-        $('#client-info').hide();
-    }
-});
-
+        // Cachez les informations du client lorsqu'on clique en dehors des marqueurs
+        $('#map-container').on('click', function(event) {
+            if (!$(event.target).closest('.interactive-point').length) {
+                $('#client-info').hide();
+            }
+        });
+    });
 </script>
 
 <?php get_footer();
