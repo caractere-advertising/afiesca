abe57f5d288a847aa459238c0c149407d2e62bac
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 10:04:17 +0200

    interactivemap

--- src/js/interactiveMap.js
+++ src/js/interactiveMap.js
@@ -1,30 +1,30 @@
-    var items = [
-        {
-            position: {
-                left: 100, // Position x du premier emplacement
-                top: 200   // Position y du premier emplacement
-            },
-            informations: "Informations sur le premier client"
+var items = [
+    {
+        position: {
+            left: 100, // Position x du premier emplacement
+            top: 200   // Position y du premier emplacement
         },
-        {
-            position: {
-                left: 300, // Position x du deuxième emplacement
-                top: 400   // Position y du deuxième emplacement
-            },
-            informations: "Informations sur le deuxième client"
+        informations: "Informations sur le premier client"
+    },
+    {
+        position: {
+            left: 300, // Position x du deuxième emplacement
+            top: 400   // Position y du deuxième emplacement
         },
-        // Ajoutez autant d'objets que nécessaire pour chaque emplacement
-    ];
+        informations: "Informations sur le deuxième client"
+    },
+    // Ajoutez autant d'objets que nécessaire pour chaque emplacement
+];
 
-    var options = {
-        allowHtml: true,
-        triggerEvent: 'click',
-        shareBox: false,
-    };
+var options = {
+    allowHtml: true,
+    triggerEvent: 'click',
+    shareBox: false,
+};
     
-    // Plugin activation
-    $(document).ready(function() {
-        $("#my-interactive-image").interactiveImage(items, options);
+// Plugin activation
+$(document).ready(function() {
+  $("#my-interactive-image").interactiveImage(items);
 
         // Gestionnaire d'événements pour afficher les informations du client
         $('#my-interactive-image').on('click', '.interactive-point', function() {
@@ -38,4 +38,4 @@
                 $('.result .card').hide();
             }
         });
-    });
+});
