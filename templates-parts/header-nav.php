7b4a405a045fc5ea99e63955ac69aa526d9834b7
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-06-05 11:10:02 +0200

    ./

--- templates-parts/header-nav.php
+++ templates-parts/header-nav.php
@@ -7,7 +7,7 @@
             $logoBlack = get_field('logo-negatif','options');
 
             if($logo || $logoBlack):
-                if(is_front_page(  ) || is_page_template( '../contact.php' )):?>
+                if(is_front_page(  ) || is_page_template( './contact.php' )):?>
                     <img src="<?php echo $logo['url'];?>" alt="<?php echo $logo['title'];?>" />
                 <?php else :?>
                     <img src="<?php echo $logoBlack['url'];?>" alt="<?php echo $logoBlack['title'];?>"/>
