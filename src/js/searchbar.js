9d7d12ae9b8c0448f3ef61acb259ff4928eea85e
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-06 14:39:41 +0100

    searchbar.js

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -7,7 +7,7 @@ $("#searchbar").keyup(function (e) {
     $.ajax({
       type: "POST",
       datatype: "json",
-      url: "https://afi-esca.be/wp-admin/admin-ajax.php",
+      url: "https://afi-esca.be/wgk2bwa8hk9ogzt/admin-ajax.php",
       data: {
         action: "searchbar",
         q: value,
